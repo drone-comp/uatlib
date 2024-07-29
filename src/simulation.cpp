8784ebcd28a369c77cd474eae8879f764964c622
Filipe Verri <filipeverri@gmail.com>
2024-07-29 15:21:34 -0300

    Documenting and some organizing in simulation.hpp.

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -18,6 +18,8 @@ auto agents_private_status_fn::operator()(id_t id) const -> agent_private_status
 
 auto agents_private_status_fn::active_count() const -> uint_t { return active_.size(); }
 
+auto agents_private_status_fn::active() const -> const std::vector<id_t>& { return active_; }
+
 void agents_private_status_fn::insert(any_agent a)
 {
   active_.push_back(first_id_ + agents_.size());
@@ -48,6 +50,16 @@ auto agents_private_status_accessor::at(agents_private_status_fn& self, id_t id)
   assert(id >= self.first_id_);
   assert(id - self.first_id_ < self.agents_.size());
   return self.agents_[id - self.first_id_];
+}
+
+auto agents_private_status_accessor::insert(agents_private_status_fn& self, any_agent a) const -> void
+{
+  self.insert(std::move(a));
+}
+
+auto agents_private_status_accessor::update_active(agents_private_status_fn& self, std::vector<id_t> new_agents) const -> void
+{
+  self.update_active(std::move(new_agents));
 }
 
 } // namespace uat
